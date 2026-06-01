57af4e2f725d09d84d021ee8e2b9a81ac8ebd23f
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-05-31 21:45:19 -0500

    Fix: Incluir archivos de Proyectos_java correctamente

---
+++ Proyectos_java/src/Contacto/User.java
@@ -0,0 +1,17 @@
+package Contacto;
+
+public class User {
+    private String nombre;
+    private String telefono;
+
+    public User(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+}
