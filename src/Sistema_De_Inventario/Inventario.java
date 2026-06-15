624aa48afc0bf00015ce07f5535e52dd84d447e0
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-06-15 17:51:49 -0500

    Creacion de sistema de inventario

--- src/Sistema_De_Inventario/Inventario.java
+++ src/Sistema_De_Inventario/Inventario.java
@@ -1,4 +1,27 @@
 package Sistema_De_Inventario;
 
+import java.util.HashMap;
+
 public class Inventario {
+    HashMap<Integer, Producto> inventario;
+
+    public Inventario(){
+        this.inventario = new HashMap<>();
+    }
+
+    public void Agregar(Producto producto){
+        inventario.put(producto.getId(), producto);
+    }
+
+    public Producto buscar(int id){
+        return inventario.get(id);
+    }
+
+    public boolean eliminar(int id){
+        if (inventario.containsKey(id)){
+            inventario.remove(id);
+            return true;
+        }
+        return false;
+    }
 }
